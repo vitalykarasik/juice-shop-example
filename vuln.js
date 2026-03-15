2ce5f47428e4131331d6bcfc409107318d11ec95
vitalykarasik <me@vitalykarasik.com>
2026-03-15 10:31:56 +0200

    added vuln.js

---
+++ vuln.js
@@ -0,0 +1,11 @@
+{
+ "ruleId": "js/xss",
+ "level": "error",
+ "message": { "text": "Cross-site scripting vulnerability" },
+ "locations": [{
+   "physicalLocation": {
+     "artifactLocation": { "uri": "routes/user.js" },
+     "region": { "startLine": 45 }
+   }
+ }]
+}
